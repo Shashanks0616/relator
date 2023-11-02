345a4657e6965fc9c9d4acccdfeb7c29d4421c88
Shashank0616 <shashanksingh0616@gmail.com>
2023-11-02 20:36:18 +0530

    create the Oauth file

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,15 +7,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAxR4JGnWKSkRdbfi1sG6k3k-feZn8BNLM",
-  authDomain: "relator-clone-58aa1.firebaseapp.com",
-  projectId: "relator-clone-58aa1",
-  storageBucket: "relator-clone-58aa1.appspot.com",
-  messagingSenderId: "675746580128",
-  appId: "1:675746580128:web:b25c975275fc054b99ebc5",
-  measurementId: "G-YP3SWGQG08"
+  apiKey: "AIzaSyAxqYOdEWWJijjeU_syF_hYqsTMCpLoHl8",
+  authDomain: "relatorclone.firebaseapp.com",
+  projectId: "relatorclone",
+  storageBucket: "relatorclone.appspot.com",
+  messagingSenderId: "233701942562",
+  appId: "1:233701942562:web:bacf098586c6b8548df17b",
+  measurementId: "G-VYY8WP2P0N"
 };
 
 // Initialize Firebase
- initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 export const db = getFirestore()
